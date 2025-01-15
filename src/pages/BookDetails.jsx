5f2eafa3ed792b3ac369d1daae045328ad34461e
sadman7858 <sadmansakir77@gmail.com>
2025-01-16 00:50:50 +0600

    btn changes 2

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -65,7 +65,7 @@ function BookDetails() {
               </div>
               <div className={style.actions}>
                 <Button onClick={handleBack} variant='outline-danger'>
-                  Go to Back testing
+                  Go to Back
                 </Button>
               </div>
             </div>
